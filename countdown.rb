b7916522badd64aaf7d8a327ae77ddbc746b5474
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-15 17:31:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,15 +8,17 @@ def countdown(secs)
     
   secs -= 1
   end
-return "HAPPY NEW YEAR!"
+ return "HAPPY NEW YEAR!"
 end
 
 
 def countdown_with_sleep(secs)
   secs = 10
-  while secs > 0 
+   while secs > 0 
   
   puts "#{secs} SECOND(S)!"
   
   sleep(1)
+end 
+end 
   
